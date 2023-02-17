239373a28e0bcf1ff01e976ef46dd0ad5a1d6522
UshaMurugan07 <ushamurugan66@gmail.com>
2023-02-17 15:24:32 +0530

    3 methods added in blue

--- src/test/java/org/git/Class1.java
+++ src/test/java/org/git/Class1.java
@@ -10,5 +10,13 @@ System.out.println("2");
 	private void method3() {
 System.out.println("3");
 	}
-
+	private void method7() {
+System.out.println("4");
+	}
+private void method8() {
+System.out.println("5");
+}
+private void method9() {
+System.out.println("6");
+}
 }
